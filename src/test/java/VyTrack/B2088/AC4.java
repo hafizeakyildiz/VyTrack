a91960da3d40310267b3372e736912dcd2570c73
cybertekgroup10 <71042591+cybertekgroup10@users.noreply.github.com>
2020-09-09 20:25:08 -0500

    Add files via upload

---
+++ src/test/java/VyTrack/B2088/AC4.java
@@ -0,0 +1,11 @@
+package VyTrack.B2088;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AC4 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qa2.vytrack.com/");
+    }
+}
---
+++ src/test/java/VyTrack/B2088/ac41.java
@@ -0,0 +1,7 @@
+package VyTrack.B2088;
+
+public class ac41 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
