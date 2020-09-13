a26cbe247e6cb3bace2de336d871c7fddbfd09b5
cihatozcan/G <cihatozcan1903@hotmail.com>
2020-09-13 10:21:40 -0500

    secondtest

--- src/test/java/VyTrack/B2088/AC4_CO.java
+++ src/test/java/VyTrack/B2088/AC4_CO.java
@@ -3,7 +3,7 @@ package VyTrack.B2088;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AC4 {
+public class AC4_CO {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://qa2.vytrack.com/");
