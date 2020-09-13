de8b96d96860efa2c2d1b2fce77767c3d98f46e0
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-09-13 13:44:23 -0500

    push test

--- src/test/java/VyTrack/B2087/AC6.java
+++ src/test/java/VyTrack/B2087/AC6.java
@@ -1,7 +1,14 @@
 package VyTrack.B2087;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AC6 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qa2.vytrack.com/");
+
     }
 }
